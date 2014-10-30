2185977cf2a566fe51248b6ea7ab89320510ad81
Espen Almdahl <espen.almdahl@hyper.no>
2014-10-30 10:02:37 +0100

    add ALL THE THINGS

---
+++ app/src/main/java/no/hyper/dateintervalpicker/CalendarAdapter.java
@@ -0,0 +1,184 @@
+package no.hyper.dateintervalpicker;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.SortedMap;
+
+/**
+ * Created by espenalmdahl on 28/10/14.
+ */
+public class CalendarAdapter extends BaseAdapter {
+
+    private Calendar calendar;
+    private Locale locale = Locale.getDefault();
+    private ArrayList<String> days;
+    private ArrayList<String> dates;
+    private HashMap<Integer, LinearLayout> containers;
+    private int visibleMonth;
+    private int visibleYear;
+    private Context context;
+    private int startPosition;
+
+    public CalendarAdapter(Context context, int resource) {
+        this.context = context;
+        calendar = Calendar.getInstance(Locale.getDefault());
+        calendar.setTime(new Date());
+        locale = new Locale("NO");
+
+        //first set day initials for use in top row
+        days = new ArrayList<String>();
+        DateFormatSymbols symbols = new DateFormatSymbols(locale);
+        String[] dayNames = symbols.getShortWeekdays();
+        for ( int i = 0 ; i < dayNames.length; i++) {
+            int index = (i + calendar.getFirstDayOfWeek()) % dayNames.length;
+            if (index > 0) {
+                days.add("" + dayNames[index].charAt(0));
+            }
+        }
+        dates = new ArrayList<String>();
+        containers = new HashMap<Integer, LinearLayout>();
+        buildArray(calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR));
+
+    }
+
+
+    public static class ViewHolder {
+        public final TextView date;
+
+        public ViewHolder(TextView dView) {
+            this.date = dView;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return dates.size();
+    }
+
+    @Override
+    public LinearLayout getItem(int i) {
+        return containers.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView dateView;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context)
+                    .inflate(R.layout.date_item, parent, false);
+
+            dateView = (TextView) convertView.findViewById(R.id.date);
+            convertView.setTag(new ViewHolder(dateView));
+        } else {
+            ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+            dateView = viewHolder.date;
+        }
+        String content = dates.get(position);
+        dateView.setText(content);
+
+        if ( content.equals("") || position < 7) {
+            dateView.setTextColor(Color.LTGRAY);
+            convertView.setEnabled(false);
+        }
+        else {
+            dateView.setTextColor(Color.DKGRAY);
+            convertView.setEnabled(true);
+
+        }
+        containers.put(position, (LinearLayout) convertView);
+        return convertView;
+    }
+
+
+    public void buildArray(int month, int year) {
+        visibleMonth = month;
+        visibleYear = year;
+
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+
+        dates.clear();
+        startPosition = 7; //minumum starting position is Monday after day row.
+
+        for (LinearLayout ll : containers.values()) {
+            ll.setBackgroundColor(Color.WHITE);
+        }
+
+        //add day names for first row
+        for (String day : days) {
+            dates.add(day);
+        }
+
+        //find first day of month
+        calendar.set(Calendar.DAY_OF_MONTH,1);
+        int firstDay = calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        int emptySpaces = (firstDay - calendar.getFirstDayOfWeek());
+        if (emptySpaces < 0) emptySpaces = 7 - Math.abs(emptySpaces);
+
+        for (int i = 0 ; i < emptySpaces; i++) {
+            dates.add("");
+            startPosition++;
+        }
+        for (int i = calendar.get(Calendar.DAY_OF_MONTH); i <= calendar.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            dates.add("" + i);
+        }
+    }
+
+    public void nextMonth() {
+        if (calendar.get(Calendar.MONTH) == Calendar.DECEMBER) {
+            buildArray(Calendar.JANUARY, calendar.get(Calendar.YEAR) + 1);
+        }
+        else {
+            buildArray(calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR));
+        }
+        notifyDataSetChanged();
+    }
+
+    public void previousMonth() {
+        if (calendar.get(Calendar.MONTH) == Calendar.JANUARY) {
+            buildArray(Calendar.DECEMBER, calendar.get(Calendar.YEAR) - 1);
+        }
+        else {
+            buildArray(calendar.get(Calendar.MONTH) - 1, calendar.get(Calendar.YEAR));
+        }
+        notifyDataSetChanged();
+    }
+
+    public String getMonthString() {
+        return calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, locale);
+    }
+
+    public String getYearString() {
+        return "" + calendar.get(Calendar.YEAR);
+    }
+
+    public int getStartPosition() {
+        return startPosition;
+    }
+}
--- app/src/main/java/no/hyper/dateintervalpicker/DateIntervalPicker.java
+++ app/src/main/java/no/hyper/dateintervalpicker/DateIntervalPicker.java
@@ -1,17 +1,51 @@
 package no.hyper.dateintervalpicker;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 
-public class DateIntervalPicker extends Activity {
+public class DateIntervalPicker extends Activity implements View.OnTouchListener{
+
+    private GridView calendar;
+    private CalendarAdapter adapter;
+
+    private Date fromDate, toDate;
+
+    private int downPos, upPos;
+    private ArrayList<LinearLayout> selectedItems;
+    private int swipe;
+    private static final int LEFT = -1;
+    private static final int NONE = 0;
+    private static final int RIGHT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_interval_picker);
+        calendar = (GridView) findViewById(R.id.calendar_grid);
+
+        adapter = new CalendarAdapter(this,
+                R.layout.date_item);
+
+        calendar.setAdapter(adapter);
+        calendar.setOnTouchListener(this);
+        selectedItems = new ArrayList<LinearLayout>();
     }
 
 
@@ -33,4 +67,109 @@ public class DateIntervalPicker extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void changeMonth(View v) {
+        if (v.getId() == R.id.next_month) {
+            adapter.nextMonth();
+        }
+        else if (v.getId() == R.id.prev_month) {
+            adapter.previousMonth();
+        }
+        ((TextView) findViewById(R.id.month)).setText(adapter.getMonthString() + adapter.getYearString());
+    }
+
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent e) {
+
+        if (e.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            clearSelected();
+            downPos = calendar.pointToPosition((int) e.getX(), (int) e.getY());
+            if (downPos >= adapter.getStartPosition()) {
+                selectItem(downPos);
+            }
+            return true;
+        }
+        if (e.getActionMasked() == MotionEvent.ACTION_MOVE) {
+            int pos = calendar.pointToPosition((int)e.getX(),(int) e.getY());
+
+            if (pos > downPos) {
+                swipe = RIGHT;
+                clearSelected();
+                for (int i = downPos; i <= pos; i++) {
+                    selectItem(i);
+                }
+            }
+            //pos is -1 if inside grid but on empty cell
+            else if (pos < downPos && pos > -1) {
+                swipe = LEFT;
+                clearSelected();
+                //reverse adding to have the lowest date at index 0
+                for (int i = pos; i <= downPos; i++) {
+                    selectItem(i);
+                }
+            }
+            paintSelected();
+            return true;
+        }
+        if (e.getActionMasked() == MotionEvent.ACTION_UP) {
+            swipe = NONE;
+            if ( ! selectedItems.isEmpty() ) {
+                CharSequence dateDown = ((TextView) selectedItems.get(0).findViewById(R.id.date)).getText();
+                CharSequence dateUp = ((TextView) selectedItems.get(selectedItems.size() - 1).findViewById(R.id.date)).getText();
+                Toast.makeText(this, dateDown + ". til " + dateUp + ". " + adapter.getMonthString(), Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    private void selectItem(int position) {
+        LinearLayout ll = (LinearLayout) calendar.getItemAtPosition(position);
+        if ( ll != null && ll.isEnabled()) {
+            selectedItems.add(ll);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void paintSelected() {
+        if ( ! selectedItems.isEmpty() ) {
+            if (selectedItems.size() == 1) {
+                View v = selectedItems.get(0);
+                v.setBackground(getResources().getDrawable(R.drawable.selected_single_item));
+                ((TextView) v.findViewById(R.id.date)).setTextColor(Color.WHITE);
+            } else {
+                View v = selectedItems.get(0);
+                v.setBackground(getResources().getDrawable(R.drawable.selected_left_edge));
+                ((TextView) v.findViewById(R.id.date)).setTextColor(Color.WHITE);
+                for (int i = 1; i < selectedItems.size() - 1; i++) {
+                    v = selectedItems.get(i);
+
+                    //make rounded corners on the edges of each week
+                    if (calendar.getPositionForView(v) % 7 == 6) {
+                        v.setBackground(getResources().getDrawable(R.drawable.selected_right_edge));
+                    } else if (calendar.getPositionForView(v) % 7 == 0) {
+                        v.setBackground(getResources().getDrawable(R.drawable.selected_left_edge));
+                    } else {
+                        v.setBackground(getResources().getDrawable(R.drawable.selected));
+                    }
+                    ((TextView) v.findViewById(R.id.date)).setTextColor(Color.WHITE);
+                }
+                v = selectedItems.get(selectedItems.size() - 1);
+                v.setBackground(getResources().getDrawable(R.drawable.selected_right_edge));
+                ((TextView) v.findViewById(R.id.date)).setTextColor(Color.WHITE);
+            }
+        }
+    }
+
+    private void clearSelected() {
+        for ( LinearLayout ll : selectedItems ) {
+            ll.setBackgroundColor(Color.WHITE);
+            ((TextView) ll.findViewById(R.id.date)).setTextColor(Color.DKGRAY);
+        }
+        selectedItems.clear();
+    }
+
 }
